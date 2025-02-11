470bd5ad9296f9918d0290b85b7d2745fa5dfa88
Raju donkeshwar <dvijaychandrakumar@gmail.com>
2025-02-11 21:25:16 +0530

    second commite

--- main.tf
+++ main.tf
@@ -105,3 +105,45 @@ resource "aws_nat_gateway" "example" {
   # on the Internet Gateway for the VPC.
   depends_on = [aws_internet_gateway.main]
 }
+
+
+
+
+
+
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    var.public_route_table_tags,
+    {
+      Name = "${local.resource_name}-public"
+    }
+  )
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    var.private_route_table_tags,
+    {
+      Name = "${local.resource_name}-private"
+    }
+  )
+}
+
+resource "aws_route_table" "database" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    var.database_route_table_tags,
+    {
+      Name = "${local.resource_name}-database"
+    }
+  )
+}
--- variables.tf
+++ variables.tf
@@ -77,3 +77,24 @@ variable "nat_gateway_tags" {
     default = {}
   
 }
+
+
+variable "public_route_table_tags" {
+
+    default = {}
+  
+}
+
+
+variable "private_route_table_tags" {
+
+    default = {}
+  
+}
+
+
+variable "database_route_table_tags" {
+
+    default = {}
+  
+}
